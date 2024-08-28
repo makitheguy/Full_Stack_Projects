0f215deb8f98a1ce2e9a5f29542ae9427f53411b
Madhvendra singh <makitheguy@gmail.com>
2024-08-28 20:42:39 +0530

    Add files via upload

---
+++ Day1_hello_with_DateTime/Day1_hello_with_DateTime/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Day1_hello_with_DateTime project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Day1_hello_with_DateTime.settings')
+
+application = get_asgi_application()
